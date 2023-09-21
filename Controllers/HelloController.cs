8b55a2f48f81759d44e398f38839500dfd687673
Anderson Alves <andso.alves@gmail.com>
2023-09-21 17:01:25 -0300

    HelloAPI

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace helloWorld.Controllers
+{
+    [Route("~/")]
+    [ApiController]
+    public class HelloController
+    {
+        [HttpGet]
+        public string Hello()
+        {
+            return "Hello World";
+        }
+
+        [HttpGet("/meunome")]
+        public string MeuNome()
+        {
+            return "Chamando URI /meunome";
+        }
+    }
+}
